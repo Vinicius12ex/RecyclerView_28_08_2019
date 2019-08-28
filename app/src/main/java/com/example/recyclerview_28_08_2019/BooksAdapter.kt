19c9ca4122e00ad118863e3a179003f54b45b3c8
Vinicius Aguiar <vinicius12ex@hotmail.com>
2019-08-28 10:05:50 -0300

    list implemented

--- app/src/main/java/com/example/recyclerview_28_08_2019/BooksAdapter.kt
+++ app/src/main/java/com/example/recyclerview_28_08_2019/BooksAdapter.kt
@@ -1,10 +1,43 @@
 package com.example.recyclerview_28_08_2019
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-class BooksAdapter(){
+class BooksAdapter() : ListAdapter<Book, BooksAdapter.BookViewHolder>(BookDiffUtils()){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_book_layout, null)
+        return BookViewHolder(view);
+    }
+
+    override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
     inner class  BookViewHolder(view : View) : RecyclerView.ViewHolder(view){
-        
+        private val title : TextView=view.findViewById(R.id.title)
+        private val page : TextView=view.findViewById(R.id.pages)
+        private val description : TextView=view.findViewById(R.id.description)
+
+        fun bind(book:Book){
+            title.text = book.title
+            page.text = book.pageCount.toString()
+            description.text = book.shortDescription
+        }
+    }
+
+    class BookDiffUtils: DiffUtil.ItemCallback<Book>() {
+        override fun areItemsTheSame(oldItem: Book, newItem: Book): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Book, newItem: Book): Boolean {
+            return oldItem.title == newItem.title
+        }
+
     }
 }
--- app/src/main/java/com/example/recyclerview_28_08_2019/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview_28_08_2019/MainActivity.kt
@@ -2,22 +2,31 @@ package com.example.recyclerview_28_08_2019
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.InputStream
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    val json = readJSONFromAsset()
+
+
+    val type = object : TypeToken<List<Book>>() {}.type
+    val books = Gson().fromJson<List<Book>>(json, type)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val json = readJSONFromAsset()
-
-
-        val type = object : TypeToken<List<Book>>() {}.type
-        val books = Gson().fromJson<List<Book>>(json, type)
-
+        val layoutManager = LinearLayoutManager(this)
+        recyclerview.layoutManager = layoutManager
+        val adapter = BooksAdapter()
+        recyclerview.adapter = adapter
+        adapter.submitList(books)
+        
     }
 
     private fun readJSONFromAsset(): String? {
